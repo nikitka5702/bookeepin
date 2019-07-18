d66246d4e3694c3163de88e9b9c7fed72681522a
nikitka5702 <niksav98@gmail.com>
2019-07-18 13:12:24 +0300

    Initial commit

---
+++ front/src/Router.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// pages go here
+
+const Router = props => {
+  return (
+    <Switch>
+
+    </Switch>
+  )
+}
+
+export default Router
---
+++ front/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloLink, concat } from 'apollo-link'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import App from './App'
+
+import './index.css'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql',
+})
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('token')
+  operation.setContext({
+    headers: {
+      authorization: token ? `JWT ${token}` : '',
+    }
+  })
+
+  return forward(operation)
+})
+
+const client = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache(),
+  fetchOptions: {
+    mode: 'no-cors'
+  }
+})
+
+const app = (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+)
+
+ReactDOM.render(<App />, app);
