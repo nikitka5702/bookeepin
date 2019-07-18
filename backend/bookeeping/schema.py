259c3261007a68036a0ad52c187d1574a45dc736
nikitka5702 <niksav98@gmail.com>
2019-07-18 16:37:52 +0300

    Set up django static to nginx

---
+++ backend/bookeeping/schema.py
@@ -0,0 +1,19 @@
+import graphene
+import graphene_jwt
+
+
+class Query(
+    graphene.ObjectType
+):
+    pass
+
+
+class Mutation(
+    graphene.ObjectType
+):
+    token_auth = graphene_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphene_jwt.Verify.Field()
+    refresh_token = graphene_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
