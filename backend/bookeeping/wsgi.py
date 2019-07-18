ca38f3966ef54aa0186529c322d0fabe9ac0aa19
nikitka5702 <niksav98@gmail.com>
2019-07-18 14:37:25 +0300

    docker-compose fixes

--- backend/bookeeping/wsgi.py
+++ backend/bookeeping/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookeepin project.
+WSGI config for bookeeping project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookeepin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookeeping.settings')
 
 application = get_wsgi_application()
--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookeepin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookeeping.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
